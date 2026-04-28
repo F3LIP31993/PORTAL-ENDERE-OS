ed80e40bd299fd2ba88569fbdb5e13a54d7d1f11
felipe <felipe.rhuan2707@gmail.com>
2026-04-28 12:13:23 -0300

    chore: auto sync 2026-04-28 12:13:22

--- start-auto-sync-com-push.cmd
+++ start-auto-sync-com-push.cmd
@@ -2,6 +2,6 @@
 setlocal
 cd /d "%~dp0"
 
-powershell -NoProfile -ExecutionPolicy Bypass -File ".\auto-git-sync.ps1" -RepoPath "." -Branch "main" -PollSeconds 10 -DebounceSeconds 120 -PushToRemote
+powershell -NoProfile -ExecutionPolicy Bypass -File ".\auto-git-sync.ps1" -RepoPath "." -Branch "main" -PollSeconds 5 -DebounceSeconds 20 -PushToRemote
 
 endlocal
